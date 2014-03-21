9243f06864f5a715d571af41d74e2ba25dbd8949
Dmitry <cell.8184160900@yahoo.com>
2014-03-21 00:19:29 -0700

    Create def_objects.py

---
+++ def_objects.py
@@ -0,0 +1,8 @@
+        #print 'Logo Dicts!!!'
+    my_dict_Logo = dict()
+    for i in xrange(29):
+        my_dict_Logo['keyLogo' + str(i+1)] = ".//*[@id='logoMenu']/select/option[" + str(i+1) + "]"
+    #print my_dict_Logo
+    for key in my_dict_Logo:
+        print key
+    print 'Logo tested' 
