6cf4c02f8e3cb30cf38c0031d26635c9c2201e6b
Dmitry <cell.8184160900@yahoo.com>
2014-09-11 10:17:03 -0700

    Update select_profile_avatars.py

--- select_profile_avatars.py
+++ select_profile_avatars.py
@@ -2,5 +2,5 @@
     selenium_driver.find_element(By.XPATH, ".//*[@id='choiceMenu']/ul/li[2]/a").click()
     my_dict_GirlImage = dict()
     for i in range(9):
-        my_dict_GirlImage['KeyGirlImage' + str(i+1)] = ".//*[@id='girlImage']/img[" + str(i+1) + ']'
-    print "Girl Images tested"
+        my_dict_GirlImage['KeyGirlImage' + str(i+1)] = ".//*[@id='blahImage']/img[" + str(i+1) + ']'
+    print "blah Images tested"
