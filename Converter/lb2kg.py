0cea9bb86446d3e612d7ab9112a0b80956f44a2d
Dmitry <cell.8184160900@yahoo.com>
2014-10-08 21:26:39 -0700

    Create lb2kg.py

---
+++ Converter/lb2kg.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# weights calculator
+#convert inch and pounds to centimeters and kilograms
+ 
+# 1 Inch = 2.54 Centimeters
+
+def inch2cm():
+    try:
+        inpt1 = int(raw_input("type number of inch and i will convert it to centimeters: "))
+        cm = inpt1 * 2.54
+        print str(cm) + ' cm'
+    except ValueError:
+        print("That's not a number!")
