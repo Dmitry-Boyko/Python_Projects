7fd80094ecbc65df84d8618284ba67f97e4ffaf8
Dmitry <cell.8184160900@yahoo.com>
2014-11-26 23:26:04 -0800

    Update ExplicitWaits_vs_ImplicitWaits.py

--- ExplicitWaits_vs_ImplicitWaits.py
+++ ExplicitWaits_vs_ImplicitWaits.py
@@ -1,14 +1,22 @@
 ### Explicit Waits ###
 '''
-An explicit waits is code you define to wait for a certain condition to occur before proceeding further in the code. The worst case of this is time.sleep(), which sets the condition to an exact time period to wait. There are some convenience methods provided that help you write code that will wait only as long as required. WebDriverWait in combination with ExpectedCondition is one way this can be accomplished.
-from selenium.webdriver.support import expected_conditions as EC
+An explicit waits is code you define to wait for a certain condition to occur 
+before proceeding further in the code. The worst case of this is time.sleep(), 
+which sets the condition to an exact time period to wait. There are some 
+convenience methods provided that help you write code that will wait only as 
+long as required. WebDriverWait in combination with ExpectedCondition is one 
+way this can be accomplished.
 '''
+from selenium.webdriver.support import expected_conditions as EC
 wait = WebDriverWait(driver, 10)
 element = wait.until(EC.element_to_be_clickable((By.ID,'someid')))
 
 ### Implicit Waits ###
 '''
-An implicit wait is to tell WebDriver to poll the DOM for a certain amount of time when trying to find an element or elements if they are not immediately available. The default setting is 0. Once set, the implicit wait is set for the life of the WebDriver object instance.
+An implicit wait is to tell WebDriver to poll the DOM for a certain amount of 
+time when trying to find an element or elements if they are not immediately available. 
+The default setting is 0. Once set, the implicit wait is set for the life of the 
+WebDriver object instance.
 '''
 from selenium import webdriver
 driver = webdriver.Firefox()
