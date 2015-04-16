c7bfaee09c7a339099d1f0fcf0643f9e35d79ff5
Dmitry <8184160900.cell@gmail.com>
2015-04-15 22:41:16 -0700

    Update Verify_element_by_text.py

--- Verify_element_by_text.py
+++ Verify_element_by_text.py
@@ -1,6 +1,6 @@
 # Verify_element_by_text
         element = 'Deployment Usage and Aging Report'
-        selenium_driver.find_element(By.CSS_SELECTOR, '#LAB_CONF_USAGE_AGING_panelTitle')
+        selenium_driver.find_element(By.CSS_SELECTOR, '#<..>')
         if element != u'Deployment Usage and Aging Report':
             print "Text verification Failed: element text is not %r" % element
         else:
