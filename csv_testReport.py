a84195a261e628b31cac849d93c24a03ee184f53
Dmitry <8184160900.cell@gmail.com>
2015-07-10 23:48:07 -0700

    Update csv_testReport.py

--- csv_testReport.py
+++ csv_testReport.py
@@ -1,3 +1,4 @@
+### csv_generator.py
 import csv
 import sys
 
@@ -5,3 +6,20 @@ def csv_File():
     with open('test_report.scv', 'w') as csvfile:
         test_writer = csv.DictWriter(csvfile, delimiter = ',')
         test_writer.writerow(['Test Case'] + ['Result'])
+
+### File "test_engine"
+from csv_generator import csv_File
+
+class address_book():
+    def addressBook(self, selenium_driver):
+        try:
+            selenium_driver.find_element(By.ID, "userAcctTab_MainMenu").click()
+            selenium_driver.find_element(By.LINK_TEXT, "Contacts").click()
+            print('AddressBook URL selected')
+            test_writer = csv.writer(csvFile, delimiter=',',  quoting=csv.QUOTE_MINIMAL) #quotechar='|',
+            test_writer.writerow(['\nNavigate to AddressBook'] + ['Pass'])
+
+        except:
+            traceback.print_exc()
+            test_writer = csv.writer(csvFile, delimiter=',',  quoting=csv.QUOTE_MINIMAL) #quotechar='|',
+            test_writer.writerow(['\nNavigate to AddressBook'] + ['Fail'])
