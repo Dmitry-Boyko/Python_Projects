7344d88f597f79952f33385ea48d6ce70a6ae207
Dmitry <8184160900.cell@gmail.com>
2015-08-02 20:35:31 -0700

    Update URL_fetcher.py

--- URL_fetcher.py
+++ URL_fetcher.py
@@ -1,3 +1,5 @@
+# V1
+
 import urllib2
 response = urllib2.urlopen("http://www.cinemark.com/home.aspx")
 # html = response.read()
@@ -6,3 +8,16 @@ for line in response.readlines():
     print line
 print "Done!"
 response.close()
+
+# V2
+
+import csv
+import urllib2
+
+downloaded_data  = urllib2.urlopen("http://www.cinemark.com/home.aspx")
+csv_data = csv.reader(downloaded_data)
+
+for row in csv_data:
+    print row
+
+downloaded_data.close()
