d9a6a1b25e7bbd3f4d7bc492420852fda28bf56a
Dmitry <8184160900.cell@gmail.com>
2015-08-18 22:24:27 -0700

    Update Verify_element_by_text.py

--- Verify_element_by_text.py
+++ Verify_element_by_text.py
@@ -8,11 +8,14 @@
             print('Deployment Usage and Aging Report - verified')
 
 # Verify word in sentence exist
-        sentence = "I hope you're having a great day, my name is Ja’Rel Rucker and I’m a Recruiter for @WalmartLabs"
-        selenium_driver.find_element(By.CSS_SELECTOR, '#<..>')
-        for elem in sentence:
-                if elem.text == u"hope":
-                        print('Element "hope" in text found, test pass')
-                else:
-                        selenium_driver.refresh()
+
+from selenium.webdriver.support.ui import WebDriverWait
+
+sentence = WebDriverWait(driver, 10).\
+        until(lambda driver: selenium_driver.find_element(By.ID, 'custom_text'))
+for elem in sentence:
+        if elem.text == u"hope":
+                print('Element "hope" in text found, test pass')
+        else:
+                selenium_driver.refresh()
                         
