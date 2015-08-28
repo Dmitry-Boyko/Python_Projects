b7a910053b1ac0c21456e43975e6ca4eff853bc2
Dmitry <8184160900.cell@gmail.com>
2015-08-28 01:28:52 -0700

    Update IP_address_random.py

--- IP_address_random.py
+++ IP_address_random.py
@@ -1,11 +1,17 @@
 import random
 
-ip = ".".join(map(str, (random.randint(0, 255)
-                        for _ in range(4))))
+print("test 1")
+ip = ".".join(map(str, (random.randint(0, 255)      # IP's min and max number
+                        for _ in range(4))))     # will gererate 4 group of numbers "xxx.xxx.xxx.xxx"
+print ip
+
 
 # or
+print("\ntest 2")
 
-for x in xrange(1,100):
-  ip = "170.12."
-  ip += ".".join(map(str, (random.randint(0, 255) 
-                          for _ in range(2))))
+
+for x in xrange(1,10):                                  # generated randomaly 10 ip addresses
+    ip = "170.12."                                      # IP address will begin ...
+    ip += ".".join(map(str, (random.randint(0, 255)     # IP's min and max number
+                          for _ in range(2))))          # will generate last two group of numbers "xxx.xxx"
+    print ip
