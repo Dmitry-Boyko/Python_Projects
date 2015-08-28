a52d4762d4f6d78ba3764c58c259893ed25fb8e2
Dmitry <8184160900.cell@gmail.com>
2015-08-28 01:05:35 -0700

    Update IP_address_random.py

--- IP_address_random.py
+++ IP_address_random.py
@@ -2,3 +2,10 @@ import random
 
 ip = ".".join(map(str, (random.randint(0, 255)
                         for _ in range(4))))
+
+# or
+
+for x in xrange(1,100):
+  ip = "170.12."
+  ip += ".".join(map(str, (random.randint(0, 255) 
+                          for _ in range(2))))
