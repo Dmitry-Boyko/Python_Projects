865d2f0f8fb10f68ee5bf7849dc46fc041f22620
Dmitry <8184160900.cell@gmail.com>
2015-09-24 12:40:33 -0700

    Update jQuery_with_webdriver.py

--- jQuery_with_webdriver.py
+++ jQuery_with_webdriver.py
@@ -1,6 +1,6 @@
 selenium_driver.execute_script('jQuery("#dialog_ok_button").click();')
 
-jQuery(".cssmenu li a:contains('Adjust Time Remaining')").click();
+selenium_driver.execute_script('jQuery(".cssmenu li a:contains('Adjust Time Remaining')").click();')
 
 unction deleteTest() {
   jQuery("a[id*=deleteEventOrAccess]").click();
