101a7827fc59278ae01d7ed226b97a379ecd7f95
Dmitry <8184160900.cell@gmail.com>
2015-11-29 20:32:56 -0800

    Update setup_date.py

--- Setup_Date/setup_date.py
+++ Setup_Date/setup_date.py
@@ -1,10 +1,17 @@
 # Send date to text-box
-
+import time
 import datetime
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-## Data tuday
-selenium_driver.find_element(By.ID, "text-box Date").send_keys(time.strftime("%d/%m/%Y")) 
+def driver():
+    selenium_driver = webdriver.Firefox()
 
-## Data tomorrow
-td = datetime.datetime.now() + datetime.timedelta(days=1)
-selenium_driver.find_element(By.ID, "text-box Date").send_keys((td.strftime("%m/%d/%Y")))
+def set_a_date(selenium_driver):
+
+    ## Data tuday
+    selenium_driver.find_element(By.ID, "text-box Date").send_keys(time.strftime("%d/%m/%Y"))
+
+    ## Data tomorrow
+    td = datetime.datetime.now() + datetime.timedelta(days=1)
+    selenium_driver.find_element(By.ID, "text-box Date").send_keys((td.strftime("%m/%d/%Y")))
