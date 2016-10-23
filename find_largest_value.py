cab5ade89e74c6293e3254c3a17b0fb379404522
boyd <8184160900.cell@gmail.com>
2016-10-23 21:51:14 +0000

    pushing all

--- find_largest_value.py
+++ find_largest_value.py
@@ -7,3 +7,12 @@ for the_num in [9, 13, 46, 23, 86, 43, 16]:
     print largest_so_far, the_num
     
 print 'After', largest_so_far
+
+# Summing elements in the list by loop
+print 'Summing elements in the list by loop'
+zork = 0
+print 'Before', zork
+for thing in [9, 13, 46, 23, 86, 43, 16]:
+    zork = zork + thing
+    print zork, thing
+print 'After', zork
