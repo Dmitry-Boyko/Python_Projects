508644e158fa49bbd56bcee4b38c45b21b5c9917
boyd <8184160900.cell@gmail.com>
2016-10-24 04:30:16 +0000

    add one more loop

--- find_largest_value.py
+++ find_largest_value.py
@@ -1,18 +1,47 @@
-largest_so_far = -1
-print 'Before', largest_so_far
+myList = [9, 13, 46, 23, 86, 43, 16]
 
-for the_num in [9, 13, 46, 23, 86, 43, 16]:
-    if the_num > largest_so_far:
-        largest_so_far = the_num
-    print largest_so_far, the_num
+def largeestNum(some_value):
+    largest_so_far = -1
+    print 'Before', largest_so_far
     
-print 'After', largest_so_far
+    for the_num in some_value:
+        if the_num > largest_so_far:
+            largest_so_far = the_num
+        print largest_so_far, the_num
+        
+    print 'After', largest_so_far
 
-# Summing elements in the list by loop
-print 'Summing elements in the list by loop'
-zork = 0
-print 'Before', zork
-for thing in [9, 13, 46, 23, 86, 43, 16]:
-    zork = zork + thing
-    print zork, thing
-print 'After', zork
+def sumList(some_value):
+    # Summing elements in the list by loop
+    print 'Summing elements in the list by loop'
+    zork = 0
+    print 'Before', zork
+    for thing in some_value:
+        zork = zork + thing
+        print zork, thing
+    print 'After', zork
+
+# Finding the Average in a Loop
+def averg(some_value):
+    print('Finding the Average in a Loop')
+    count = 0
+    sum = 0
+    print 'Before', count, sum
+    
+    for value in some_value:
+        count = count + 1
+        sum  = sum + value
+        print count, sum, value
+    print 'After', count, sum, sum / count
+
+def filteringLoop(some_value):
+    print 'Before'
+    for value in some_value:
+        if value > 20:
+            print 'Large number', value
+    print 'Afrter'
+
+largeestNum(myList)
+sumList(myList)
+averg(myList)
+filteringLoop(myList)
