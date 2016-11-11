ba29698b3a05f817fc4b5b39b13bc50abf6e1078
boyd <8184160900.cell@gmail.com>
2016-11-11 06:51:13 +0000

    added reverse

--- lessons/revers_string.py
+++ lessons/revers_string.py
@@ -11,17 +11,33 @@ sentx = {
 
 print "Selected second word: ", sentx[str2][::-1] + sentx[str3] + sentx[str1]
 
+# -----------------------------------------------------------------------------------
 
-def reverse(data_list): 
+def reverse1(data_list): 
     length = len(data_list) 
     s = length 
     
-    new_list = [None]*lenght
+    new_list = [None]*length
     
     for item in data_list: 
         s = s - 1 
         data_list[s] = item 
     return data_list
+# -----------------------------------------------------------------------------------
 
-def reverse(data_list):  # Slice Method
+def reverse_a_string_more_slowly(data_list):
+    new_strings = []
+    index = len(data_list)
+    while index:
+        index -= 1                       
+        new_strings.append(data_list[index])
+    return ''.join(new_strings)
+
+# -----------------------------------------------------------------------------------
+def reverse2(data_list):  # Slice Method
 	return data_list[::-1]
+# -----------------------------------------------------------------------------------
+
+print reverse_a_string_more_slowly('tseb uoy')
+	
+print reverse2('tseb uoy')
