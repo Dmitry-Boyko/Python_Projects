c7ae844b41e18c20cc52da87b23f4eeebc4a9895
boyd <8184160900.cell@gmail.com>
2017-08-24 22:24:20 +0000

    cels

--- lessons/celsius2fahrenheit.py
+++ lessons/celsius2fahrenheit.py
@@ -1,3 +1,6 @@
+
+print('ex 01')
+
 def c2f(cels):
     tf = tc * 1.8 + 32
     return tf
@@ -5,3 +8,7 @@ def c2f(cels):
 tc = input("Please add Temperature in Celsius: ")
 
 print(c2f(tc))
+
+print('\nex 02')
+money={"saving_account":200, "checking_account":100, "under_bed":[500,10,100]}
+print(money['under_bed'][1])
