f15b7af9e13ec14994630869dfd6c9477f13132c
boyd <8184160900.cell@gmail.com>
2017-09-08 02:39:14 +0000

    lb 2 kg

--- Converter/fr2cl.py
+++ Converter/fr2cl.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# weights calculator
-#convert inch and pounds to centimeters and kilograms
+#convert Fahrenheit to Celsius formula  
+# T(°C) = (T(°F) - 32) / 1.8
 
 # C = 5/9 (F-32)
 
@@ -9,6 +9,6 @@ def fr2cl():
     try:
         inpt3 = int(raw_input("type temperature in Fahrenheits and i will convert it to Celsius: "))
         c =  0.5555555555555 * (inpt3 - 32.0)
-        print str(c) + 'C'
+        print str(c) + ' C'
     except ValueError:
         print("That's not a number!")
--- Converter/lb2kg.py
+++ Converter/lb2kg.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # weights calculator
-#convert inch and pounds to centimeters and kilograms
- 
-# 1 Inch = 2.54 Centimeters
+#convert inch to centimeters
+# 1 lb = 0.45359237 kg
 
-def inch2cm():
+def lb2kg():
     try:
-        inpt1 = int(raw_input("type number of inch and i will convert it to centimeters: "))
-        cm = inpt1 * 2.54
-        print str(cm) + ' cm'
+        inpt = int(raw_input("type number of inch and i will convert it to centimeters: "))
+        kg = inpt * 0.45359237
+        print str(kg) + ' kg'
     except ValueError:
         print("That's not a number!")
