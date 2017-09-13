04c565459741841dc14a735cfe77ac03fb0c2c49
boyd <8184160900.cell@gmail.com>
2017-09-13 03:01:58 +0000

    d

--- ForLoop_Format/forLoopFormat001.py
+++ ForLoop_Format/forLoopFormat001.py
@@ -26,3 +26,25 @@ email_domain = ['gmail', 'hotmail', 'yahoo']
 
 for i, j in zip(names, email_domain):
     print(i, j)
+
+print('\n')
+
+temperatures=[10,-20,-289,100]
+def c_to_f(c):
+    if c > -273.15:
+        f=c*9/5+32
+        return f
+    else:
+        return "That temperature doesn't make sense!"
+for t in temperatures:
+    print(c_to_f(t))
+#If your version looked like below, that's still correct:
+temperatures=[10,-20,-289,100]
+def c_to_f(c):
+    if c< -273.15:
+        return "That temperature doesn't make sense!"
+    else:
+        f=c*9/5+32
+        return f
+for t in temperatures:
+    print(c_to_f(t))
