cbbcf9bccd3041a874986b328f339329fdf08321
boyd <8184160900.cell@gmail.com>
2018-09-06 18:34:46 +0000

    rr

---
+++ _quick_stars/quickStart.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+
+driver = webdriver.Firefox()
+
+driver.implicitly_wait(15)
+
+driver.get("http://www.google.com")
+searchField = driver.find_element_by_css_selector("input[name=q]")
+
+driver.quit()
