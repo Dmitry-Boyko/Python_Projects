b26beca57e2246ab077d593aefa1af4c25db2582
boyd <8184160900.cell@gmail.com>
2018-09-18 04:29:32 +0000

    d

--- Prime_numbers/primeNumber.py
+++ Prime_numbers/primeNumber.py
@@ -13,3 +13,17 @@ for num in range(2,101):
     if all(num%i!=0 for i in range(2,num)):
        print num
 
+print "\nfrom user input\n"
+
+number = int(raw_input("Pleae add some number here: > "))
+
+if number > 1:
+    for i in range(2, number):
+        if (number % i) == 0:
+            print number, "is not a prime number"
+            break
+        else:
+            print "Yes", number, " is prime number!"
+            break
+else:
+    print "something going wrong"
