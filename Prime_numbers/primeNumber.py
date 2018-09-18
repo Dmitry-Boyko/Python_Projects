8251577eebeca0e05d84ddcab702cf380b76eea7
boyd <8184160900.cell@gmail.com>
2018-09-18 03:34:23 +0000

    d

---
+++ Prime_numbers/primeNumber.py
@@ -0,0 +1,14 @@
+for num in range(2,101):    # to iterate between 10 to 20
+    prime = True
+    for i in range(2,num):  # to iterate on the factors of the number
+        if (num%i==0):      # to determine the first factor
+            prime = False
+    if prime:
+       print num
+       
+       
+#  You can write the same much shorter and more pythonic:
+
+for num in range(2,101):
+    if all(num%i!=0 for i in range(2,num)):
+       print num
