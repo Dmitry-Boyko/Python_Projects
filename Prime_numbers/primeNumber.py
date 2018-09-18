b140e88b9b73186fce063ae02f3215cc82130fb1
boyd <8184160900.cell@gmail.com>
2018-09-18 04:30:15 +0000

    d

--- Prime_numbers/primeNumber.py
+++ Prime_numbers/primeNumber.py
@@ -6,13 +6,14 @@ for num in range(2,101):    # to iterate between 10 to 20
     if prime:
        print num
        
-print "Another variation\n"       
+print "Another variation\n"     
+# -------------------------------------------------------------
 #  You can write the same much shorter and more pythonic:
 
 for num in range(2,101):
     if all(num%i!=0 for i in range(2,num)):
        print num
-
+# -------------------------------------------------------------
 print "\nfrom user input\n"
 
 number = int(raw_input("Pleae add some number here: > "))
